14b16356492897cf81a8959e35bb0f5738d1f0bb
mbwq <josaphat.mauricio@gmail.com>
2025-09-17 15:05:49 -0300

    ajout de route vers les page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './components/homepage/homepage';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component:Homepage},
+    { path: 'Login', component: Login} //la route login(page de connexion)
+];
