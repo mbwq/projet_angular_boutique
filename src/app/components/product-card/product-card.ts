3f4f0b8ec5c812d428f4c4a49d3553d43c897ddc
mbwq <josaphat.mauricio@gmail.com>
2025-09-26 17:26:30 -0300

    ajoute de product card

---
+++ src/app/components/product-card/product-card.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.scss'
+})
+export class ProductCard {
+  @Input() product: any;
+
+  constructor() { }
+
+  ngOnInit(): void{}
+}
