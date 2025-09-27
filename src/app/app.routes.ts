94e1e7d4ae4926ad7d38e7ec2f58fb05222be38e
mbwq <josaphat.mauricio@gmail.com>
2025-09-27 18:11:08 -0300

    ajoute de la route pour signin et modif de la navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './components/homepage/homepage';
 import { Login } from './components/login/login';
+import { Signin } from './components/signin/signin';
 
 export const routes: Routes = [
 
     { path: '', component:Homepage}, //route par defaut
-    { path: 'Login', component:Login} //la route login(page de connexion)
+    { path: 'Login', component:Login}, //la route login(page de connexion)
+    { path: 'Signin', component:Signin} //la route sign-in(page de creation de compte)
 ];
